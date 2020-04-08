23110957d31989d5920f61a683badd9af5ba7531
Hermoult <adrienhergat@gmail.com>
2020-04-08 15:27:45 +0200

    maintenance.php

--- class/maintenance.php
+++ class/maintenance.php
@@ -1,14 +1,38 @@
 <?php
     class maintenance extends bateau{
 
-        // Les parametres-----------------------------------------------------------------
+        // Les parametres-----------------------------------------------------------------        
+        /**
+         *
+         * @var string id de la maintenance
+         * @var string date maintenance electrique
+         * @var string date maintenance electronique
+         * @var string date maintenance des circuits
+         * @var string date maintenance de la coque
+         * @var string date maintenance de l'agrement
+         */
         private $idmaintenance ='';
         private $electricite ='';
         private $electronique ='';
         private $circuit ='';
         private $coque ='';
         private $grement ='';
+        //Le constructeur-----------------------------------------------------------------
 
+
+
+
+        //Les Setteur---------------------------------------------------------------------
+
+
+
+
+        //Les Getteurs--------------------------------------------------------------------
+
+
+
+
+        //Les methodes--------------------------------------------------------------------
 
 
         public function hydrate(array $donnees)
