72afd21f71cccd7a3b55401b0ceb023862233a2c
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-08 15:39:25 +0200

    _push

--- class/gps.php
+++ class/gps.php
@@ -3,9 +3,9 @@ class gps extends bateau
 {
 
     // Les parametres-----------------------------------------------------------------
-    private $idgps = '';
-    private $longitude = '';
-    private $latitude = '';
+    private $_idgps = '';
+    private $_longitude = '';
+    private $_latitude = '';
 
 
     //Le constructeur-----------------------------------------------------------------
@@ -15,13 +15,68 @@ class gps extends bateau
 
     //Les Setteur---------------------------------------------------------------------
 
+    /**
+     * Set the value of _idgps
+     *
+     * @return  self
+     */
+    public function set_idgps($_idgps)
+    {
+        $this->_idgps = $_idgps;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of _longitude
+     *
+     * @return  self
+     */
+    public function set_longitude($_longitude)
+    {
+        $this->_longitude = $_longitude;
+
+        return $this;
+    }
 
 
+    /**
+     * Set the value of _latitude
+     *
+     * @return  self
+     */
+    public function set_latitude($_latitude)
+    {
+        $this->_latitude = $_latitude;
+
+        return $this;
+    }
 
     //Les Getteurs--------------------------------------------------------------------
 
+    /**
+     * Get the value of _idgps
+     */
+    public function get_idgps()
+    {
+        return $this->_idgps;
+    }
 
+    /**
+     * Get the value of _longitude
+     */
+    public function get_longitude()
+    {
+        return $this->_longitude;
+    }
 
+    /**
+     * Get the value of _latitude
+     */
+    public function get_latitude()
+    {
+        return $this->_latitude;
+    }
 
     //Les methodes--------------------------------------------------------------------
 
