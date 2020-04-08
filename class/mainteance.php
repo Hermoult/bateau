350876acf636bbf3fac1ecc3c5842b46d611afd1
Hermoult <adrienhergat@gmail.com>
2020-04-08 15:17:48 +0200

    class maintenance

---
+++ class/mainteance.php
@@ -0,0 +1,11 @@
+<?php
+    class maintenance{
+
+        // Les parametres-----------------------------------------------------------------
+        private $idmaintenance ='';
+        private $electricite ='';
+        private $electronique ='';
+        private $circuit ='';
+        private $coque ='';
+        private $grement ='';
+    }
